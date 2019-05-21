fc735c19d66e846159ce11a91ebd4bc0a3f97b10
Jason Victorio <jgvictorio@gmail.com>
2019-05-22 10:31:43 +1200

    update article list and article view

--- Blog/Blog.Master.designer.cs
+++ Blog/Blog.Master.designer.cs
@@ -22,6 +22,15 @@ namespace Blog {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
+        /// Header control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder Header;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
