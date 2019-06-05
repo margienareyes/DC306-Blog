54b010a2f765545dd533a5a0e39ae0a5978667a0
Margiena Reyes <47847348+margienareyes@users.noreply.github.com>
2019-06-05 15:35:38 +1200

    login by  username & validation

--- Blog/Login.aspx.designer.cs
+++ Blog/Login.aspx.designer.cs
@@ -22,13 +22,13 @@ namespace Blog {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// txtEmail control.
+        /// txtUsername control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+        protected global::System.Web.UI.WebControls.TextBox txtUsername;
         
         /// <summary>
         /// txtPassword control.
