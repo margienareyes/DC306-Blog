36157089d3941d815a10737a5ce546b292481b20
Margiena Reyes <47847348+margienareyes@users.noreply.github.com>
2019-06-10 14:32:03 +1200

    AuthorListView

--- Blog/BlogAuthorList.aspx.cs
+++ Blog/BlogAuthorList.aspx.cs
@@ -4,13 +4,29 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.Configuration;
 
 namespace Blog
 {
     public partial class BlogAuthorList : System.Web.UI.Page
     {
+        private SqlConnection SqlConnection;
+        private SqlCommand SqlCommand;
+        string connectionString = WebConfigurationManager.ConnectionStrings["Blog"].ConnectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            this.SqlConnection = new SqlConnection(this.connectionString);
+            this.SqlConnection.Open();
+            this.SqlCommand = new SqlCommand("SELECT AuthorId, Name, Username, Email FROM Author ORDER BY AuthorId", this.SqlConnection);
+            this.SqlCommand.ExecuteNonQuery();
+            DataSet ds = new DataSet();
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(this.SqlCommand);
+            sqlDataAdapter.Fill(ds);
+            this.DataList1.DataSource = ds;
+            this.DataList1.DataBind();
 
         }
     }
