2ce2b8907e05da7a9b61b9089bfee7b0393fd41c
Jason Victorio <jgvictorio@gmail.com>
2019-06-17 01:10:49 +1200

    db: update article

--- Blog/dashboard/article.aspx.cs
+++ Blog/dashboard/article.aspx.cs
@@ -15,18 +15,67 @@ namespace Blog
     {
         private SqlConnection SqlConnection;
         string connectionString = WebConfigurationManager.ConnectionStrings["Blog"].ConnectionString;
+        public string id;
+        SqlDataReader SqlDataReader;
 
         protected void Page_Load(object sender, EventArgs e)
         {
             this.SqlConnection = new SqlConnection(this.connectionString);
+            this.id = Request.QueryString["id"];
+            
+            if (IsPostBack)
+            {
+                return;
+            }
 
+            if (id != null)
+            {
+
+                SqlCommand SqlCommand = new SqlCommand("SELECT " +
+                    "Article.ArticleId as ArticleId, Article.ImagePath as ImagePath, Article.Title as Title, Article.Date as Date, Author.Name as Author, Article.Excerpt as Excerpt, Article.Content as Content  " +
+                    "FROM Article " +
+                    "INNER JOIN Author ON Article.AuthorId = Author.AuthorId " +
+                    "WHERE ArticleId=@ArticleId", SqlConnection);
+                SqlCommand.Parameters.AddWithValue("@ArticleId", id);
+
+                try
+                {
+                    SqlConnection.Open();
+                    this.SqlDataReader = SqlCommand.ExecuteReader();
+                    while (this.SqlDataReader.Read())
+                    {
+                        textboxTitle.Text = this.SqlDataReader["Title"].ToString();
+                        textboxExcerpt.Text = this.SqlDataReader["Excerpt"].ToString();
+                        textboxContent.Text = this.SqlDataReader["Content"].ToString();
+                    }
+                }
+
+                catch (Exception exception)
+                {
+
+                }
+
+                SqlConnection.Close();
+            }
         }
 
         protected void buttonSave_Click(object sender, EventArgs e)
         {
-            SqlCommand SqlCommand = new SqlCommand("" +
-                "INSERT INTO Article (Title, Content, ImagePath, AuthorId, Date, Excerpt) VALUES (@Title, @Content, @ImagePath, @AuthorId, @Date, @Excerpt);",
-                this.SqlConnection);
+            SqlCommand SqlCommand;
+            if (this.id != null)
+            {
+                SqlCommand = new SqlCommand("" +
+                    "UPDATE Article " +
+                    "SET Title=@Title, Excerpt=@Excerpt " +
+                    "WHERE ArticleId=@ArticleId",
+                    this.SqlConnection);
+                SqlCommand.Parameters.AddWithValue("ArticleId", id);
+            } else
+            {
+                SqlCommand = new SqlCommand("" +
+                    "INSERT INTO Article (Title, Content, ImagePath, AuthorId, Date, Excerpt) VALUES (@Title, @Content, @ImagePath, @AuthorId, @Date, @Excerpt);",
+                    this.SqlConnection);
+            }
 
             string filename = fileUploadImage.FileName;
             string path = Server.MapPath("../public/");
@@ -34,9 +83,13 @@ namespace Blog
             SqlCommand.Parameters.AddWithValue("@Title", this.textboxTitle.Text);
             SqlCommand.Parameters.AddWithValue("@Content", this.textboxContent.Text);
             SqlCommand.Parameters.AddWithValue("@AuthorId", Session["AuthorId"]);
-            SqlCommand.Parameters.AddWithValue("@ImagePath", "/public/" + filename);
             SqlCommand.Parameters.AddWithValue("@Date", DateTime.Now);
             SqlCommand.Parameters.AddWithValue("@Excerpt", this.textboxExcerpt.Text);
+
+            if (filename != null)
+            {
+                SqlCommand.Parameters.AddWithValue("@ImagePath", "/public/" + filename);
+            }
 
             this.SqlConnection.Open();
             try
@@ -51,8 +104,15 @@ namespace Blog
                         }
                     }
 
-                    this.labelStatus.Text = "Article saved succesfully";
-                    this.clearForm();
+                    if (id != null)
+                    {
+                        this.labelStatus.Text = "Article updated succesfully";
+                    }
+                    else
+                    {
+                        this.labelStatus.Text = "Article saved succesfully";
+                        this.clearForm();
+                    }
                 }
             }
             catch (Exception exception)
@@ -67,6 +127,7 @@ namespace Blog
         {
             this.textboxTitle.Text = "";
             this.textboxContent.Text = "";
+            this.textboxExcerpt.Text = "";
         }
     }
 }
