d7ab923435a42b311e2a3b15652d7d2f0274f268
Jason Victorio <jgvictorio@gmail.com>
2019-06-16 15:22:33 +1200

    update dashboard articles, fix dashboard masterpage

--- Blog/article.aspx.cs
+++ Blog/article.aspx.cs
@@ -4,14 +4,46 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.Configuration;
 
 namespace Blog
 {
     public partial class BlogArticleView : System.Web.UI.Page
     {
+        private SqlConnection SqlConnection;
+        private SqlCommand SqlCommand;
+        string connectionString = WebConfigurationManager.ConnectionStrings["ArtSchoolDB"].ConnectionString;
+        SqlDataReader SqlDataReader;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            //string id = Request.QueryString["id"]; 
+            //this.SqlConnection = new SqlConnection(this.connectionString);
+            //this.SqlCommand = new SqlCommand("SELECT Id, day, time, topic, tutor FROM Schedule WHERE Id=@id", this.SqlConnection);
+            //this.SqlCommand.Parameters.AddWithValue("@id", id);
 
+            //try
+            //{
+            //    this.SqlConnection.Open();
+            //    this.SqlDataReader = this.SqlCommand.ExecuteReader();
+            //    while (this.SqlDataReader.Read())
+            //    {
+            //        this.textboxDay.Text = this.SqlDataReader["day"].ToString();
+            //        this.textboxTime.Text = this.SqlDataReader["time"].ToString();
+            //        this.textboxTopic.Text = this.SqlDataReader["topic"].ToString();
+            //        this.textboxTutor.Text = this.SqlDataReader["tutor"].ToString();
+            //        this.textboxId.Text = this.SqlDataReader["Id"].ToString();
+            //    }
+            //}
+
+            //catch (Exception exception)
+            //{
+            //    this.labelStatus.Text = exception.Message;
+            //}
+
+            //this.SqlConnection.Close();
         }
     }
 }
--- Blog/dashboard/authors.aspx
+++ Blog/dashboard/authors.aspx
@@ -2,4 +2,22 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Repeater runat="server" ID="DataList1">
+        <ItemTemplate> 
+            <div class="col-lg-8">
+			    <div class="card">
+				    <div class="card-header d-flex align-items-end">
+                        <div class="mr-auto">
+					        <h2><%# Eval("AuthorId") %></h2> 
+                            <span><%# Eval("Name") %> - <%# Eval("Username") %></span>
+                        </div>
+                        <div>
+                            <button class="btn btn-primary" type="button">Edit</button>
+                            <button class="btn btn-danger" type="button">Delete</button>
+                        </div>
+				    </div>
+			    </div>
+            </div>
+        </ItemTemplate>
+    </asp:Repeater>
 </asp:Content>
